7c50f17b77c661fa8718c912ef7cb273386d0e25
Ryhor Volasau <G.volosov@gmail.com>
2023-06-10 16:30:45 +0400

    feat: add apiKey and project source packages

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9e3d958f2fe14cb3bcbb741514245db5', // получите свой ключ https://newsapi.org/
         });
     }
 }
