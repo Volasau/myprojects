2ffd5155485aeaa29ebc936f3a6f9568430187f2
Ryhor Volasau <G.volosov@gmail.com>
2023-06-10 23:00:56 +0400

    feat: change loader.js on loader.ts and appLoader.js on appLoader.ts

--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -1,25 +1,56 @@
-import News from '../view/news/news';
-import Sources from '../view/sources/sources';
-import { NewsItem, NewsItems, NewsSources, NewsSource } from '../../types/index';
+export interface RequestOptions {
+    [key: string]: unknown;
+}
 
-export class AppView {
-    private news: News;
-    private sources: Sources;
+type Callback<T> = (data: T) => void;
 
-    constructor() {
-        this.news = new News();
-        this.sources = new Sources();
+class Loader {
+    private baseLink: string;
+    private options: RequestOptions;
+
+    constructor(baseLink: string, options: RequestOptions) {
+        this.baseLink = baseLink;
+        this.options = options;
     }
 
-    drawNews(data: NewsItems): void {
-        const values: NewsItem[] = data?.articles || [];
-        this.news.draw(values);
+    getResp<T>(
+        { endpoint, options = {} }: { endpoint: string; options?: RequestOptions },
+        callback: Callback<T> = () => {
+            console.error('No callback for GET response');
+        }
+    ): void {
+        this.load<T>('GET', endpoint, callback, options);
     }
 
-    drawSources(data: NewsSources): void {
-        const values: NewsSource[] = data?.sources || [];
-        this.sources.draw(values);
+    errorHandler(res: Response): Response {
+        if (!res.ok) {
+            if (res.status === 401 || res.status === 404) {
+                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+            }
+            throw new Error(res.statusText);
+        }
+
+        return res;
+    }
+
+    makeUrl(options: RequestOptions, endpoint: string): string {
+        const urlOptions = { ...this.options, ...options };
+        let url = `${this.baseLink}${endpoint}?`;
+
+        Object.keys(urlOptions).forEach((key) => {
+            url += `${key}=${urlOptions[key]}&`;
+        });
+
+        return url.slice(0, -1);
+    }
+
+    load<T>(method: string, endpoint: string, callback: Callback<T>, options: RequestOptions = {}): void {
+        fetch(this.makeUrl(options, endpoint), { method })
+            .then(this.errorHandler)
+            .then((res) => res.json())
+            .then((data: T) => callback(data))
+            .catch((err) => console.error(err));
     }
 }
 
-export default AppView;
+export default Loader;
