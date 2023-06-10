6feaa318c7916a81bebec653cc417f9393e6e211
Ryhor Volasau <G.volosov@gmail.com>
2023-06-10 22:50:26 +0400

    feat: change loader.js on loader.ts

---
+++ migration-to-ts/src/components/controller/loader.ts
@@ -0,0 +1,25 @@
+import News from '../view/news/news';
+import Sources from '../view/sources/sources';
+import { NewsItem, NewsItems, NewsSources, NewsSource } from '../../types/index';
+
+export class AppView {
+    private news: News;
+    private sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: NewsItems): void {
+        const values: NewsItem[] = data?.articles || [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: NewsSources): void {
+        const values: NewsSource[] = data?.sources || [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
