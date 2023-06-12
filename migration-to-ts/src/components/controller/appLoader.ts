b649538ca90aeeb6195c833fcb48b9009727c9ab
Ryhor Volasau <G.volosov@gmail.com>
2023-06-12 14:47:02 +0400

    feat: change URL in appLoader file

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '9e3d958f2fe14cb3bcbb741514245db5', // получите свой ключ https://newsapi.org/
         });
     }
