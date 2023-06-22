fdc4ecb566334038775c738cb494e0795f6320aa
Ryhor Volasau <G.volosov@gmail.com>
2023-06-22 16:48:02 +0400

    feat: add packages for task

---
+++ rss-css-selectors/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.html$/i,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+            favicon: './src/assets/klever.png',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new FaviconsWebpackPlugin({
+            logo: './src/assets/klever.png',
+            // outputPath: path.resolve(__dirname, './src/index'),
+            publicPath: './',
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
